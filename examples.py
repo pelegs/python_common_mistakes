483c9757665c55db760981618cf7d3d3f065f010
Peleg Sapir <pelegs@gmail.com>
2022-10-14 21:52:12 +0200

    First commit

--- examples.py
+++ examples.py
@@ -112,6 +112,33 @@ print(f'a={a}, b={b}, c={c}')
 
 # ------------------------------------------------ #
 
+#######################
+# Lists, tuples, sets #
+#######################
+
+# Lists
+def somefunc():
+    pass
+lst = [1, 'a', True, 'cow', [0, 1, 2, 3], 2, somefunc, 5]
+print(lst)
+lst[2] = not(lst[2])
+lst[-1] *= 3
+del lst[-2]
+print(lst)
+
+# Tuples
+tup = (1, 5, 'b', False, (0, 0, 0), [1, 1, 1])
+print(tup)
+# tup[1] += 5 # oops
+
+# Sets
+st = set([1, 2, 3, 1, 2, 1, 1, 3, 4, 5, 1, 2, 1, 1, 5])
+print(st)
+# st[1] = -3 # oops
+st2 = set([True, (1,1,1), (1,1,1), None, (1,1,1,1), False, (1,1,1), (1,1,1,1), True, None, None])
+print(st2)
+# ------------------------------------------------ #
+
 #############
 # For loops #
 #############
@@ -157,6 +184,23 @@ colors_list = ['Orange', 'Green', 'Blue', 'Purple', 'Pink', 'Red', 'White']
 for word, animal, person, color in zip(words_list, animals_list, devops_list, colors_list):
     print(word, animal, person, color)
 
+# Iterating over dictionaries
+capitals = {
+    'Germany': 'Berlin',
+    'France': 'Paris',
+    'Italy': 'Rome',
+    'Canada': 'Ottawa',
+    'Sweden': 'Stockholm',
+    'The Netherlands': 'not the Hague',
+}
+# don't
+# for country in capitals:
+    # print(f'The capital of {country} is {capitals[country]}.')
+
+# do
+for country, city in capitals.items():
+    print(f'The capital of {country} is {city}.')
+
 # ------------------------------------------------ #
 
 #################
@@ -176,3 +220,89 @@ if isinstance(b, str):
 else:
     print(f'{b} is not a string')
 
+# ------------------------------------------------ #
+
+#################
+# File handling #
+#################
+
+# Do NOT use open() and close() - instead use 'with'
+with open('example.txt', 'r') as file:
+    txt = file.read()
+print(txt)
+
+# Split lines directly into a list
+with open('example.txt', 'r') as file:
+    txt = file.readlines()
+print(txt)
+
+# Same for writing a file
+txt = """
+It was the dawn of the third age of mankind.
+Ten years after the Earth Minbari war... the Babylon project was dream giving form.
+It's goal to prevent another war by creating a place where humans and aliens could work out their differences peacefully.
+It's a port of call, home away from home for diplomas, hustlers, entrepreneurs, and wanders.
+Humans and aliens wrap in two million five hundred thousand tons of spinning metal, all alone in the night.
+It can be a dangerous place, but it's our last best hope for peace.
+This is the story of last of the Babylon stations.
+The year is 2258 - the name of the place is Babylon 5.
+"""
+with open('output.txt', 'w') as file:
+    file.write(txt)
+
+# ------------------------------------------------ #
+
+##################
+# Comprehensions #
+##################
+
+# List comprehensions
+xs = [1, 2, 3, 4, 5, 6, 7]
+ys = [x**2 for x in xs]
+print(ys)
+
+# Nested loops inside comprehension
+nums = [1, 2, 3, 4, 5]
+letters = ['a', 'b', 'c', 'd', 'e']
+lst = [(n, l) for n in nums for l in letters]
+print(lst)
+
+# 1-to-1 matching inside comprehension
+nums = [1, 2, 3, 4, 5]
+letters = ['a', 'b', 'c', 'd', 'e']
+lst = [(n, l) for n, l in zip(nums, letters)]
+print(lst)
+
+# If conditionals in comprehensions
+even_nums = [n for n in range(20) if n%2==0]
+print(even_nums)
+
+jumping_nums = [n if n%2==0 else -n for n in range(20)]
+print(jumping_nums)
+
+# More complex example
+# This:
+word_count = []
+with open('example.txt', 'r') as file:
+    txt = file.readlines()
+for line in txt:
+    word_count.append(len(line.split()))
+print(f'Word count using the boring way: {word_count}')
+
+# ...can be written succinctly as:
+with open('example.txt', 'r') as file:
+    word_count = [len(line.split()) for line in file.readlines()]
+print(f'Word count using list comprehension: {word_count}')
+
+# Dictionary comprehensions
+devops_list = ['Aaron', 'Alemseged', 'Emanuel', 'Frauke', 'Nehru', 'Peleg', 'Satya']
+colors_list = ['Orange', 'Green', 'Blue', 'Purple', 'Pink', 'Red', 'White']
+devops_cols = {dev: col for dev, col in zip(devops_list, colors_list)}
+print(devops_cols)
+
+# A bit more complex?
+# Create a dict which contains the first word of each line as key
+# and the length of each line as value.
+with open('example.txt', 'r') as file:
+    lines_len = {line.split()[0]: len(line.split()) for line in file.readlines()}
+print(lines_len)
