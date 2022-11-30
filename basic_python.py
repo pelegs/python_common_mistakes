73bef661b35083f3226a472177a4d513546a2b7f
pelegs <pelegs@gmail.com>
2022-11-30 23:26:23 +0100

    Fixed some PEP8 issues (not all)

--- basic_python.py
+++ basic_python.py
@@ -10,8 +10,9 @@ print(n, x, z, s, b)
 # print(s + str(n))
 
 # ----- Strings ----- #
-txt1 = "I'm a text, and I can use single quotes. Double quotes, i.e. \", must be escaped."
-txt2 = 'I\'m also a text! Here\'s a quote: "bla bla bla".'
+txt1 = "I'm a text, and I can use single quotes."
+txt2 = "Double quotes, i.e. \", must be escaped."
+txt3 = 'I\'m also a text! Here\'s a quote: "bla bla bla".'
 
 mltxt1 = """
 I'm a string with several lines.
@@ -22,7 +23,8 @@ print(mltxt1)
 
 mltxt2 = '''
 I'm a another multi-line string.
-I can use single quotes because I only exist between two pairs of three consequtive quotes.
+I can use single quotes because I only exist between two pairs of three
+consequtive quotes.
 '''
 print(mltxt2)
 
@@ -37,28 +39,28 @@ print('my var is a=', a, 'and also b=', b, '.')
 # Hooray!
 # """)
 
-a = 30 # int
+a = 30  # int
 print(f'a = {a:04d}')
 
-b = 1.337 # float
+b = 1.337  # float
 print(f'b = {b:0.2f}')
 print(f'b = {b:0.5f}')
 
-pi = 3.1415926535897932384626433 # another float
+pi = 3.1415926535897932384626433  # another float
 print(f'π = {pi:0.10f}...')
 
 x = 64321277
-print(f'x = {x:e}') # scientific
-print(f'x = {x:x}') # hexadecimal
+print(f'x = {x:e}')  # scientific
+print(f'x = {x:x}')  # hexadecimal
 
 # ----- Lists and dictionaries ----- #
-l = [1, 2, -3, 'a', False, 7.5]
-print(l)
-print(l[-1])
-l += [0, 0]
-print(l)
-del(l[1])
-print(l)
+lst = [1, 2, -3, 'a', False, 7.5]
+print(lst)
+print(lst[-1])
+lst += [0, 0]
+print(lst)
+del lst[1]
+print(lst)
 
 capitals = {
     'Germany': 'Berlin',
@@ -94,25 +96,34 @@ for i in range(10):
         break
     print(i)
 
+
 # ----- Functions ----- #
 def sqr(n):
     return n**2
+
 print(sqr(10))
+
 
 def func(x, y, z=10):
     return x**2-2*y+z
+
 print(func(1, 1))
 # print(func(1, 1, 1))
 # print(func(z=2))
 
+
 def no_return(a):
     print(f'You entered "{a}"')
+
 no_return(4)
+
 
 def sum(a: int, b: int) -> int:
     return a+b
+
 test = sum(1, 4.4)
 print(type(test))
+
 
 # ----- Classes ----- #
 class Person:
@@ -124,8 +135,10 @@ class Person:
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
+
 p = Person('Danny', 'Mendel')
 print(p, p.initials)
+
 
 class Employee(Person):
     def __init__(self, first_name, last_name, company):
@@ -135,8 +148,9 @@ class Employee(Person):
     def __str__(self):
         return super().__str__() + f' works at {self.company}'
 
-e = Employee('Jane', 'Doe', 'exocad')
-print(e)
+emp = Employee('Jane', 'Doe', 'exocad')
+print(emp)
+
 
 # ----- External libraries ----- #
 import numpy as np
@@ -144,7 +158,7 @@ print(np.sqrt(100))
 
 import itertools
 X = ['a', 'b']
-for x in itertools.product(X, repeat=3): # X^3
+for x in itertools.product(X, repeat=3):  # X^3
     print(x)
 
 from itertools import permutations as perms
